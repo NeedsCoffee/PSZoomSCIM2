68571280afda03fe350c077f1075b3f754f8eed0
Nick Clark <nc17927@bristol.ac.uk>
2022-07-14 14:12:27 +0100

    append to logs

--- main.ps1
+++ main.ps1
@@ -41,7 +41,7 @@ foreach($zoomUser in $allZoomUsers){
                     $deactivated++
                     if(!($zoomUser.Active)){
                         Write-Log -Message 'Orphaned Zoom user {0} deactivated' -Arguments $zoomUser.userName -Level ACTION
-                        $zoomUser | Export-Csv -Path .\logs\zoomusers_deactivated.csv -NoTypeInformation -Append
+                        $zoomUser | Export-Csv -Path .\logs\zoomusers_deactivated.csv -NoTypeInformation -Encoding utf8 -Append
                     } elseif($script:simulationMode){
                         Write-Log -Message 'SIMULATION: Orphaned Zoom user {0} deactivated' -Arguments $zoomUser.userName -Level INFO
                     } else {
@@ -79,7 +79,7 @@ foreach($zoomUser in $allZoomUsers){
                     $deactivated++
                     if(!($zoomUser.Active)){
                         Write-Log -Message 'Zoom user {0} deactivated' -Arguments $zoomUser.userName -Level ACTION
-                        $zoomUser | Export-Csv -Path .\logs\zoomusers_deactivated.csv -NoTypeInformation
+                        $zoomUser | Export-Csv -Path .\logs\zoomusers_deactivated.csv -NoTypeInformation -Encoding utf8 -Append
                     } elseif($script:simulationMode){
                         Write-Log -Message 'SIMULATION: Zoom user {0} deactivated' -Arguments $zoomUser.userName -Level INFO
                     } else {
